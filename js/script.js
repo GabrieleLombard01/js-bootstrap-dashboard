f47604ecdc613de45e9d38c79bfb519e0cc5d91b
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-06-15 15:56:14 +0200

    js started

--- js/script.js
+++ js/script.js
@@ -1,2 +1,27 @@
 //JS RESET
 console.log('JS OK')
+
+
+//todo ----- BOTTONE FA-BELL -----:
+
+//! OPERAZIONI INIZIALI:
+
+const bellIcon = document.querySelector(".fa-bell");
+const popupMenu = document.querySelector(".poap-up");
+
+let isOpen = false;
+
+//! FUNZIONI:
+// Funzione per la gestione del click:
+bellIcon.addEventListener("click", () => {
+  isOpen = !isOpen;
+  if (isOpen) {
+    popupMenu.classList.remove("d-none");
+    popupMenu.classList.add("d-block");
+  } else {
+    popupMenu.classList.remove("d-block");
+    popupMenu.classList.add("d-none");
+  }
+});
+  
+//! LOGICA EFFETTIVA:
